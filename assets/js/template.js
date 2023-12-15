ad09f8d71177feb75f4819f914daf3f4f72fb1be
Katja Dittrich <coztomate@gmail.com>
2023-12-16 00:45:11 +0100

    231215: fixed nav bar

--- assets/js/template.js
+++ assets/js/template.js
@@ -1,38 +1,43 @@
 jQuery(document).ready(function ($) {
+    var my_nav = $('.navbar-sticky');
+    // Create a placeholder for the sticky navigation to prevent content jump
+    var nav_placeholder = $('<div>').css({height: my_nav.outerHeight(), display: 'none'}).insertBefore(my_nav);
 
-	var my_nav = $('.navbar-sticky');
-	// grab the initial top offset of the navigation 
-	var sticky_navigation_offset_top = my_nav.offset().top;
+    // grab the initial top offset of the navigation 
+    var sticky_navigation_offset_top = my_nav.offset().top;
 
-	// our function that decides weather the navigation bar should have "fixed" css position or not.
-	var sticky_navigation = function () {
-		var scroll_top = $(window).scrollTop(); // our current vertical position from the top
+    // our function that decides whether the navigation bar should have "fixed" css position or not.
+    var sticky_navigation = function () {
+        var scroll_top = $(window).scrollTop(); // our current vertical position from the top
 
-		// if we've scrolled more than the navigation, change its position to fixed to stick to top, otherwise change it back to relative
-		if (scroll_top > sticky_navigation_offset_top) {
-			my_nav.addClass('stick');
-		} else {
-			my_nav.removeClass('stick');
-		}
-	};
+        // if we've scrolled more than the navigation, change its position to fixed to stick to top
+        if (scroll_top > sticky_navigation_offset_top) {
+            my_nav.addClass('stick');
+            nav_placeholder.show(); // show placeholder to maintain layout
+        } else {
+            my_nav.removeClass('stick');
+            nav_placeholder.hide(); // hide placeholder
+        }
+    };
 
-	var initio_parallax_animation = function () {
-		$('.parallax').each(function (i, obj) {
-			var speed = $(this).data('parallax-speed');
-			if (speed) {
-				var background_pos = '-' + (window.pageYOffset / speed) + "px";
-				$(this).css('background-position', 'center ' + background_pos);
-			}
-		});
-	}
+    // parallax effect: makes the background move a bit when scrolling, otherwise fixed
+    var initio_parallax_animation = function () {
+        $('.parallax').each(function (i, obj) {
+            var speed = $(this).data('parallax-speed');
+            if (speed) {
+                var background_pos = '-' + (window.pageYOffset / speed) + "px";
+                $(this).css('background-position', 'center ' + background_pos);
+            }
+        });
+    }
 
-	// run our function on load
-	sticky_navigation();
+    // run our function on load
+    sticky_navigation();
 
-	// and run it again every time you scroll
-	$(window).scroll(function () {
-		sticky_navigation();
-		initio_parallax_animation();
-	});
-
+    // and run it again every time you scroll
+    $(window).scroll(function () {
+        sticky_navigation();
+        initio_parallax_animation();
+    });
 });
+
